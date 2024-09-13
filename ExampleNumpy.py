0d3c822c2bb23e7fe1b5f08babc7bbabf2d53283
76toan <leduytoantgbn@gmail.com>
2024-09-13 23:25:30 +0700

    MNM

--- ExampleNumpy.py
+++ ExampleNumpy.py
@@ -7,8 +7,20 @@ try:
     x2 = int(input("Nhập tọa độ x cho vecto 1: "))
     y2 = int(input("Nhập tọa độ x cho vecto 1: "))
     z2 = int(input("Nhập tọa độ x cho vecto 1: "))
-    A = np.array([x1, y1, z1])
-    B = np.array([x2, y2, z2])
+
 except :
     pass
+
+
+A = np.array([x1, y1, z1])
+B = np.array([x2, y2, z2])
+
+tichVoHuong = np.dot(A, B)
+
+daiA = np.linalg.norm(A)
+daiB = np.linalg.norm(B)
+
+cosin = tichVoHuong / (daiA * daiB)
+
+print(f"Cosin góc tạo bởi 2 vecto A và B là {cosin}")
 
